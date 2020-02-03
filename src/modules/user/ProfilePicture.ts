00da68dab27bf7cc4e7d1bfc9c75f65fb91b5a2f
RiadhT <riadh_t@iol7-183.localdomain>
2020-02-03 22:11:06 +0100

    "profilePicture upload"

---
+++ src/modules/user/ProfilePicture.ts
@@ -0,0 +1,25 @@
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { createWriteStream } from "fs";
+import {GraphQLUpload} from 'graphql-upload'
+import { Upload } from "../../types/Uploads";
+
+
+
+@Resolver()
+export class ProfilePictureResolver{
+    @Mutation(()=> Boolean)
+    async addProfilePicture(@Arg("picture",()=>GraphQLUpload)
+    {
+        createReadStream,
+        filename
+    }:Upload): Promise<boolean>{
+        return new Promise(async(resolve, reject)=>
+            createReadStream()
+            .pipe(createWriteStream(__dirname + `/../../../images/${filename}`))
+            .on("finish", ()=> resolve(true))
+            .on("error", ()=> reject(false))
+        )
+    }
+
+}
+
